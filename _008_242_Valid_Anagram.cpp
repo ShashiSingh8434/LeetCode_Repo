e0eff1d5c524b65be8696d0fcd5b5253a10ffb47
Shashi Singh <alphasingh8434@gmail.com>
2026-02-02 18:44:04 +0530

    Day - 8 | Done

--- _008_242_Valid_Anagram.cpp
+++ _008_242_Valid_Anagram.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+// 97-122  -  a-z
+bool isAnagram(string s, string t) {
+    int count[26] = {0};
+
+    for(char c : s){
+        count[int(c) - 97]++;
+    }
+    for(char c : t){
+        count[int(c) - 97]--;
+    }
+
+    for(int i : count){
+        if(i != 0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(){
+    string s = "rat";
+    string t = "car";
+    cout<<isAnagram(s,t);
     
     return 0;
 }
