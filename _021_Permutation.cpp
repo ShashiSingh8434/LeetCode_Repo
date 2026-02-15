674037f361b9cdc8712faa17b67e509f4fc95c96
Shashi Singh <alphasingh8434@gmail.com>
2026-02-15 21:51:48 +0530

    Day - 21

---
+++ _021_Permutation.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void backtrack(vector<int>& nums, vector<int>& temp, 
+                vector<vector<int>>& result, vector<bool>& used) {
+    
+    if (temp.size() == nums.size()) {
+        result.push_back(temp);
+        return;
+    }
+    
+    for (int i = 0; i < nums.size(); i++) {
+        if (used[i]) continue; 
+        temp.push_back(nums[i]);
+        used[i] = true;
+        
+        backtrack(nums, temp, result, used);
+        
+        // backtrack
+        temp.pop_back();
+        used[i] = false;
+    }
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> result;
+    vector<int> temp;
+    vector<bool> used(nums.size(), false);
+    
+    backtrack(nums, temp, result, used);
+    return result;
+}
+
+int main(){
+    
+    return 0;
+}
