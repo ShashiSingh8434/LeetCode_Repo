9497e702d449bbca753389aac73c9d0e031e46ae
Shashi Singh <alphasingh8434@gmail.com>
2026-02-20 15:11:55 +0530

    Day - 26

---
+++ _026_875_Koko_Banana.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int minEatingSpeed(vector<int>& piles, int h) {
+    int low = 1;
+    int high = *max_element(piles.begin(), piles.end());
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        long long hours = 0;
+
+        for (int pile : piles) {
+            hours += (pile + mid - 1) / mid;
+        }
+
+        if (hours <= h) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+}
+
+int main(){
+    
+    return 0;
+}
