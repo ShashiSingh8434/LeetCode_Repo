858697ca5ed0a403912ca831520d884ea687fd8c
Shashi Singh <alphasingh8434@gmail.com>
2026-05-15 19:39:22 +0530

    Day - 43

---
+++ _043_1480_Running_Sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
+
+vector<int> runningSum(vector<int>& nums) {
+    int sum = 0;
+    vector<int> ans;
+    for(int i = 0; i<nums.size(); i++){
+        sum += nums[i];
+        ans.push_back(sum);
+    }
+    return ans;
+}
