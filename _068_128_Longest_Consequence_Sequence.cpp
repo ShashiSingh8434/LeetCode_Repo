f7734a5e9de5f78d1839ead3dde78dc5cc3b2193
Shashi Singh <alphasingh8434@gmail.com>
2026-05-25 11:48:20 +0530

    Day - 52(revision)

---
+++ _068_128_Longest_Consequence_Sequence.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
+
+int longestConsecutive(vector<int>& nums) {
+    unordered_set<int> asdf;
+    int count = 0;
+
+    for(int i : nums){
+        asdf.insert(i);
+    }
+
+    for(int n : asdf){
+        int temp = 0;
+
+        if(asdf.count(n - 1)){
+            continue;
+        }
+
+        while(asdf.find(n) != asdf.end()){
+            temp++;
+            n++;
+        }
+
+        count = max(count, temp);
+    }
+
+    return count;
+}
