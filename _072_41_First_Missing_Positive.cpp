5fd43be5f7ba415591806d12eb7ca226a5fa1f53
Shashi Singh <alphasingh8434@gmail.com>
2026-05-27 15:26:06 +0530

    Day - 55

---
+++ _072_41_First_Missing_Positive.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
+
+int firstMissingPositive(vector<int>& nums) {
+    int n = nums.size();
+
+    for(int i = 0; i < n; i++) {
+        while(  nums[i] > 0 && 
+                nums[i] <= n && 
+                nums[i] != nums[nums[i] - 1]) {
+
+            swap(nums[i], nums[nums[i] - 1]);
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(nums[i] != i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+}
